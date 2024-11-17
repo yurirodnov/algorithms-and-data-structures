daeddba656eb21ee71d9699ac8022c1bbd5c42c1
yuri_rodnov <somewaystodo@yandex.ru>
2024-11-17 13:30:06 +0300

    add array code

---
+++ data-structures/Array/array.js
@@ -0,0 +1,17 @@
+const arr = [1, 2, 3, 4, 5];
+const textArr = ["a", "b", "c", "d"];
+
+// spred-operator
+// const n = [...arr];
+// console.log(n) // [ 1, 2, 3, 4, 5 ]
+
+// Clear array
+// arr.length = 0;
+
+// Stack example
+// arr.push(10);
+// arr.pop();
+
+// Queue example
+// arr.push(10)
+// arr.shift()
