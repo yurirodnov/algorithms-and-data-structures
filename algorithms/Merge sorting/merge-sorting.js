7e7319c9f4d1605526ca17de757ffdf6a940e4d8
yuri_rodnov <somewaystodo@yandex.ru>
2024-12-10 22:15:56 +0300

    add code

--- algorithms/Merge sorting/merge-sorting.js
+++ algorithms/Merge sorting/merge-sorting.js
@@ -1,49 +1,37 @@
-let iter = 0;
-
-const mergeSorting = (arr) => {
-  // Базовый случай рекурсии: если массив содержит 1 или меньше элементов, он уже отсортирован
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  // Разделяем массив на две части
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSorting(arr.slice(0, mid));
-  const right = mergeSorting(arr.slice(mid));
-  iter += 1;
-  console.log(`Iteration ${iter} left is ${left} and right is ${right}`);
-
-  // Слияние двух отсортированных частей
-  return merge(left, right);
-};
-
-const merge = (left, right) => {
-  let result = [];
+const merge = (leftSubArr, rightSubArr) => {
+  const result = [];
   let leftIndex = 0;
   let rightIndex = 0;
 
-  // Сравниваем элементы из обеих частей и собираем отсортированный массив
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
+  while (leftIndex < leftSubArr.length && rightIndex < rightSubArr.length) {
+    if (leftSubArr[leftIndex] < rightSubArr[rightIndex]) {
+      result.push(leftSubArr[leftIndex]);
+      leftIndex += 1;
     } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
+      result.push(rightSubArr[rightIndex]);
+      rightIndex += 1;
     }
   }
 
-  // Если элементы остались в одном из массивов, добавляем их в результат
-  console.log(
-    `result of merge() is ${result.concat(
-      left.slice(leftIndex),
-      right.slice(rightIndex)
-    )}`
+  return result.concat(
+    leftSubArr.slice(leftIndex),
+    rightSubArr.slice(rightIndex)
   );
-  return result.concat(left.slice(leftIndex), right.slice(rightIndex));
 };
 
-// Пример использования
-const list = [38, 27, 43, 3, 9, 82, 10];
+const mergeSorting = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSorting(arr.slice(0, mid));
+  const right = mergeSorting(arr.slice(mid));
+
+  return merge(left, right);
+};
+
+const list = [3, 1, 5, 56, 32, -6];
+
 const result = mergeSorting(list);
-console.log(result); // [3, 9, 10, 27, 38, 43, 82]
+console.log(result);
